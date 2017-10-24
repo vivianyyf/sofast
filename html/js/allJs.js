1f0b1f143cdcbe512bf0d115805ce2c3b89332ac
vivianyyf <vivianyyf@foxmail.com>
2017-10-24 17:25:09 +0800

    Revert "do css clean"
    This reverts commit 5ee9fa85a626867d913cad0290edf61f273ae1fd.

--- html/js/allJs.js
+++ html/js/allJs.js
@@ -4,8 +4,9 @@
 // document.write("<link rel = 'stylesheet' type = 'text/css' href = '/Sofast/plugin/jquery/css/jquery.dataTables.css'>");
 // document.write('<link rel="stylesheet" href="/Sofast/plugin/angular/css//scroller.dataTables.min.css">');
 document.write("<link rel = 'stylesheet' type = 'text/css' href = '/sofast/html/plugin/bootstrap/css/bootstrap.css'>");
-document.write("<link rel = 'stylesheet' type = 'text/css' href = '/sofast/html/css/site.css'>");
+document.write("<link rel = 'stylesheet' type = 'text/css' href = '/sofast/html/css/style.css'>");
 // document.write("<script  type='text/javascript' src='/sofast/html/plugin/jquery/js/jquery-1.11.3.min.js'></script>");
+
 // document.write("<script  type='text/javascript' src='/sofast/html/plugin/jquery/js/jquery.dataTables.min.js'></script>");
 document.write("<script  type='text/javascript' src='/sofast/html/plugin/angular/js/angular.js'></script>");
 // document.write("<script  type='text/javascript' src='/CSTU/System_Function/plugin/angular/js/angular-datatables.min.js'></script>");
@@ -13,8 +14,11 @@ document.write("<script  type='text/javascript' src='/sofast/html/plugin/angular
 // document.write("<script  type='text/javascript' src='/CSTU/System_Function/plugin/angular/js/angular-datatables.scroller.min.js'></script>");
 // document.write("<script  type='text/javascript' src='/CSTU/System_Function/plugin/angular/js/angular-confirm.min.js'></script>");
 document.write("<script  type='text/javascript' src='/sofast/html/plugin/bootstrap/js/ui-bootstrap-tpls-1.3.3.js'></script>");
+
 document.write("<script  type='text/javascript' src='/sofast/html/js/ui.js'></script>");
 // document.write("<script  type='text/javascript' src='/CSTU/System_Function/js/datatables.js'></script>");
+
+
 // Combiner: {
 //     "combine": "/sofast/html/css/global.css",
 //     "combine": "/sofast/html/css/layouts.css",
